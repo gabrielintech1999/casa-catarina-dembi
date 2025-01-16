0ef1c22d2d98d8d443674bbe739ce4f10dde7037
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2025-01-16 10:28:53 +0000

    carrousel component...

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,34 +1,71 @@
-import {Link} from "react-router-dom";
+// Import Swiper React components
+import {Link} from "react-router-dom"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+function Carroussel() {
+  return (
+    <Swiper
+      loop={true}
+      spaceBetween={0}
+      navigation={true}
+      slidesPerView={1}
+      modules={[Navigation, Autoplay]}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false, // Continua mesmo após interação do usuário
+      }}
+    >
+      {/* Renderizar slides dinamicamente */}
+      {Array(10)
+        .fill(0)
+        .map((_, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src="https://media.istockphoto.com/id/1413950709/photo/young-afro-woman-using-mobile-phone-at-coffee-shop.jpg?s=1024x1024&amp;w=is&amp;k=20&amp;c=bTYbxmyfRtOt8ZXen5jDw835fi1YCWh6OziUtNFivLI="
+              alt={`banner-home-${index}`}
+            />
+          </SwiperSlide>
+        ))}
+    </Swiper>
+  );
+}
 
 export default function Home() {
   return (
     <main>
-    <div className="slide">
-        <img src="https://media.istockphoto.com/id/1413950709/photo/young-afro-woman-using-mobile-phone-at-coffee-shop.jpg?s=1024x1024&amp;w=is&amp;k=20&amp;c=bTYbxmyfRtOt8ZXen5jDw835fi1YCWh6OziUtNFivLI=" alt="banner-home" />
-      </div>
-      <div className="content">
+      {/* Carrossel */}
+      <Carroussel />
 
-        {Array(10).fill(0).map((_, index) => (
-          <article className="product">
-          <Link to={`${index + 1}`}>
-            <div>
-              <img
-                src="https://m.media-amazon.com/images/I/413K0MlBc7L._SX300_SY300_QL70_FMwebp_.jpg"
-                alt="Produto"
-              />
-            </div>
-            <div className="info">
-              <div>Perfume Matte</div>
-              <div>
-                Kz <strong>15,000</strong>
-              </div>
-              <div>
-                <button className="product-btn">Comprar</button>
-              </div>
-            </div>
-          </Link>
-        </article>
-        ))}
+      {/* Produtos */}
+      <div className="content">
+        {Array(10)
+          .fill(0)
+          .map((_, index) => (
+            <article className="product" key={index}>
+              <Link to={`${index + 1}`}>
+                <div>
+                  <img
+                    src="https://m.media-amazon.com/images/I/413K0MlBc7L._SX300_SY300_QL70_FMwebp_.jpg"
+                    alt="Produto"
+                  />
+                </div>
+                <div className="info">
+                  <div>Perfume Matte</div>
+                  <div>
+                    Kz <strong>15,000</strong>
+                  </div>
+                  <div>
+                    <button className="product-btn">Comprar</button>
+                  </div>
+                </div>
+              </Link>
+            </article>
+          ))}
       </div>
     </main>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['swiper']
+  }
 })
+
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   ...
+//   optimizeDeps: {
+//     exclude: ['js-big-decimal']
+//   }
+// });
+
+
+
