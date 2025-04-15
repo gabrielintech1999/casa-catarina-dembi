9fb7b65e7569f97fb71004eef3a97ce499042213
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2025-04-15 13:02:41 +0100

    fixing the footer...

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,11 +9,7 @@ export default function Footer() {
         <a href="">Termos e Condições </a>
         <a href="">Contactos</a>
       </div>
-      <div className="flex gap-4 justify-center">
-        <a href=" https://www.facebook.com" target="_blank">Facebook </a>
-        <a href=" https://web.whatsapp.com" target="_blank">Whatsapp</a>
-        <a href=" https://www.tiktok.com" target="_blank"> Tik Tok</a>
-      </div>
+      
     </footer>
     )
   
